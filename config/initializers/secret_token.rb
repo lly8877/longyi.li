888003f9f96fc94ed17c6478ab15c789ccdc31c3
Longyi Li <longyi.li@oracle.com>
2012-08-30 23:35:30 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LongyiLi::Application.config.secret_token = '2f5fee8b0f4e7be05943b8dcda187eac9597853113e9027038e617a2e3745979baabcd2b44c605930a24af2507f8de5071d6efcc37e0b61f0c42b457fe94e19c'
