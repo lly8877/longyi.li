9a5ae6472dff6659cfdf3448551032716ea060eb
longyi <longyi@hhttech.com>
2015-06-28 16:49:03 +0800

    upgrade rails and added bootstrap

--- Gemfile
+++ Gemfile
@@ -1,6 +1,6 @@
 source 'http://rubygems.org'
 
-gem 'rails', '3.0.7'
+gem 'rails', '4.2.1'
 gem 'rest-client'
 gem 'jquery-rails'
 gem 'pg'
--- config/routes.rb
+++ config/routes.rb
@@ -66,7 +66,7 @@ LongyiLi::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => "welcome#index"
+  root :to => "home#index"
 
   # See how all your routes lay out with "rake routes"
 
